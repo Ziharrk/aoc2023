eb533e264ef38466ffe9a06b52b6b115620e2a92
Kai Prott <kai.prott@hotmail.de>
2023-12-09 15:59:21 +0100

    Day 9

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,4 +1,17 @@
 module Day9 where
 
+continueSequence :: [Integer] -> Integer
+continueSequence xs = last xs +
+  if all (== head diffs) diffs
+    then head diffs
+    else continueSequence diffs
+  where
+    diffs = getDiffs xs
+    getDiffs (x:y:zs) = y - x : getDiffs (y:zs)
+    getDiffs _ = []
+
 day9 :: IO ()
-day9 = return ()
+day9 = do
+  input <- map (map read . words) . lines <$> readFile "input/day9"
+  print $ sum $ map continueSequence input
+  print $ sum $ map (continueSequence . reverse) input
