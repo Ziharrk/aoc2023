03ad1115826c2d4b1baa698d251591a38ce6e262
Kai Prott <kai.prott@hotmail.de>
2023-12-09 16:31:01 +0100

    Add time output to tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,28 @@
 module Main(main) where
 
+import Control.Monad ( zipWithM_ )
+import Data.Time ( diffUTCTime, UTCTime (..) )
+import Data.Time.Clock.System ( getSystemTime, systemToUTCTime )
+import Data.Time.Calendar.OrdinalDate ( toOrdinalDate )
+
 import AllDays (allDays)
 
 main :: IO ()
-main = sequence_ allDays
+main = getDays >>= zipWithM_ printDay [1..]
+  where
+    getDays = do
+      t <- systemToUTCTime <$> getSystemTime
+      let (year, day) = toOrdinalDate (utctDay t)
+      if year <= 2023
+        then return $ take (day - 334) allDays
+        else return allDays
+
+    printDay :: Int -> IO () -> IO ()
+    printDay n day = do
+      let s = "Day " ++ show n
+      putStrLn s >> putStrLn (replicate (length s) '=')
+      t1 <- systemToUTCTime <$> getSystemTime
+      day
+      t2 <- systemToUTCTime <$> getSystemTime
+      putStrLn $ "Time: " ++ show (diffUTCTime t2 t1)
+      putStrLn ""
