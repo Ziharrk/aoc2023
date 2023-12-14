7700b8c0ced624635c2fe6feb0053e07713d9f2b
Kai Prott <kai.prott@hotmail.de>
2023-12-14 11:08:27 +0100

    Add day14

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,4 +1,57 @@
 module Day14 where
 
+import Data.List (transpose)
+import qualified Data.Map as Map
+
+calcLoad :: [String] -> Int
+calcLoad = go 1 0 . reverse
+  where
+    go _ acc []     = acc
+    go n acc (s:ss) = go (n+1) (acc + n * length (filter (== 'O') s)) ss
+
+-- Rolls in direction: West
+-- transpose first: North
+-- map reverse first: East
+-- transpose first and then map reverse: South
+simulateRollingWest :: [String] -> [String]
+simulateRollingWest = map column
+  where
+    column [] = []
+    column ('#':s) = '#' : column s
+    column s =
+      let (untilCube, rest) = break (== '#') s
+          numRocks = length $ filter (== 'O') untilCube
+          numPlaces = length untilCube - numRocks
+      in replicate numRocks 'O' ++ replicate numPlaces '.' ++ column rest
+
+simulateRollingCycle :: [String] -> [String]
+simulateRollingCycle =
+  transpose . map reverse . transpose . map reverse .
+  simulateRollingWest . map reverse . transpose .
+  simulateRollingWest . map reverse . transpose .
+  simulateRollingWest . transpose .
+  simulateRollingWest . transpose
+
+simulateRollingCycles :: Int -> [String] -> [String]
+simulateRollingCycles maxCy = go 0 mempty
+  where
+    go n mp ss
+      | n == maxCy = ss
+      | otherwise = case Map.lookup ss mp of
+        Nothing -> do
+          let ss' = simulateRollingCycle ss
+          go (n+1) (Map.insert ss (n, ss') mp) ss'
+        Just (n', ss') -> do
+          let cycleLen = n - n'
+              remainingCycles = (maxCy - n) `mod` cycleLen - 1
+          go (maxCy - remainingCycles) mempty ss'
+
 day14 :: IO ()
-day14 = return ()
+day14 = do
+  input <- lines <$> readFile "input/day14"
+  let rolled = transpose $ simulateRollingWest (transpose input)
+  putStr "Part 1: "
+  print $ calcLoad rolled
+  let cycled = simulateRollingCycles 1000000000 input
+  putStr "Part 2: "
+  print $ calcLoad cycled
