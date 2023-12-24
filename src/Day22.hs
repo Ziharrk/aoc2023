87b54ef5626adcd5a8b6e3d5feccf84d109925c6
Kai Prott <kai.prott@hotmail.de>
2023-12-24 09:49:03 +0100

    Add days 22 and 23

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,4 +1,80 @@
+{-# LANGUAGE PartialTypeSignatures #-}
 module Day22 where
 
+import Control.Monad.ST (runST)
+import Data.List (sortOn, nub)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (catMaybes)
+import qualified Data.Set as Set
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import Text.Parsec (Parsec, char, digit, newline, endBy1, runParser, getState, putState, many1)
+
+fallBricks :: [Brick] -> Map Int [Int]
+fallBricks bricks = runST $ do
+  v <- mk3D (succ xM) (succ yM) (succ zM) (Nothing :: Maybe Int)
+  let go [] m = return m
+      go (Brick i x1 y1 z1 x2 y2 z2 : bs) m = do
+        touching <- fitZ v i z1 (z2 - z1) [(x, y) | x <- [x1..x2], y <- [y1..y2]]
+        go bs (Map.insert i touching m)
+  go bricks Map.empty
+  where
+    (xM,yM,zM) = foldr (\(Brick _ _ _ _ x1 y1 z1) (x2, y2, z2) ->
+      (max x1 x2, max y1 y2, max z1 z2))
+      (0,0,0) bricks
+
+    mk3D x y z a = V.replicateM z (sequence (V.replicate y (MV.replicate x a)))
+
+    fitZ v i z' zSize coords = do
+      let addZ (x, y) = [ (x, y, z) | z <- [z'..z'+zSize] ]
+          zCoords = concatMap addZ coords
+      touching <- catMaybes <$> mapM (\(x, y, z) -> MV.read ((v V.! z) V.! y) x) zCoords
+      if null touching && z' > 0
+        then fitZ v i (z'-1) zSize coords
+        else mapM_ (insertAt i v) zCoords >> return (nub touching)
+
+    insertAt i v (x, y, z) = MV.write ((v V.! (z+1)) V.! y) x (Just i)
+
+chainFalling :: Map Int [Int] -> Int -> Int
+chainFalling mp = go 0 (Map.filter (not . null) mp) . Set.singleton
+  where
+    go acc m s | Just (i, is) <- Set.minView s =
+      let newM = Map.delete i $ Map.map (filter (/=i)) m
+          newFall = Map.keys (Map.filter null newM)
+      in go (acc+length newFall) (Map.filter (not . null) newM) (foldr Set.insert is newFall)
+    go acc _ _ = acc
+
 day22 :: IO ()
-day22 = return ()
+day22 = do
+  input <- readFile "input/day22"
+  case runParser (brick `endBy1` newline) 0 "" input of
+    Left err -> print err
+    Right bricks -> do
+      let sortedZ = sortOn (\(Brick _ _ _ z _ _ _) -> z) bricks
+      let supportedBy = fallBricks sortedZ
+      let oneSupport = Map.filter ((==1) . length) supportedBy
+      let notDisintegratable = nub $ concat $ Map.elems oneSupport
+      putStr "Part 1: "
+      print (length bricks - length notDisintegratable)
+      putStr "Part 2: "
+      print $ sum $ map (chainFalling supportedBy) notDisintegratable
+
+data Brick = Brick Int Int Int Int Int Int Int
+  deriving (Show, Eq)
+
+brick :: Parsec String Int Brick
+brick = mkBrick <*> number <* comma <*>
+                    number <* comma <*>
+                    number <* char '~' <*>
+                    number <* comma <*>
+                    number <* comma <*>
+                    number
+  where
+    number = read <$> many1 digit
+    comma = char ','
+    mkBrick = do
+      i <- getState
+      putState (i+1)
+      return $ \x1 y1 z1 x2 y2 z2 ->
+        Brick i (min x1 x2) (min y1 y2) (min z1 z2) (max x1 x2) (max y1 y2) (max z1 z2)
--- src/Day23.hs
+++ src/Day23.hs
@@ -1,4 +1,86 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 module Day23 where
 
+import Data.List (elemIndex)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (mapMaybe)
+import qualified Data.Set as Set
+import Data.Tuple.Extra (both, swap)
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+
+findLongestPath :: forall s. CheckSlope s => Map (Int, Int) ([(Int, Int, Int)], [(Int, Int, Int)])
+                -> (Int, Int) -> (Int, Int) -> Int
+findLongestPath mp (sX, sY) (eX, eY) = go Map.empty [(sX, sY, 0, Set.empty)]
+  where
+    go memo [] = memo Map.! (eX, eY)
+    go memo ((x, y, n, visited) : xs)
+      | (x, y) == (eX, eY) = go (Map.insertWith max (x, y) n memo) xs
+      | otherwise =
+         let visited' = Set.insert (x, y) visited
+             neigh = Map.findWithDefault ([], []) (x, y) mp
+             reachable = map (\(n', x', y') -> (x', y', n+n', visited')) $ checkSlope @s neigh
+             reachableUnvis = filter (\(x', y', _, _) -> Set.notMember (x', y') visited') reachable
+         in go (Map.insertWith max (x, y) n memo) (reachableUnvis ++ xs)
+
+-- UGLY CODE BELOW!
+mkGraph :: Vector (Vector Char) -> Map (Int, Int) ([(Int, Int, Int)], [(Int, Int, Int)])
+mkGraph mp = Map.fromList $ map junctionInfo $ Set.toList junctions
+  where
+    junctionInfo (x, y) =
+      let neighs = [ (x', y') | (x', y') <- [ (x, y-1), (x, y+1), (x-1, y), (x+1, y) ]
+                              , Just c' <- [mp V.!? y' >>= (V.!? x')]
+                              , c' /= '#' ]
+      in ((x,y), both concat $ unzip $ mapMaybe (junctionInfo' (x, y)) neighs)
+    junctionInfo' j (x, y) = do
+      (x', y', b', n) <- findNextJunction (x, y) (Set.singleton j) True 0
+      if b' then return ([(n, x', y')], [(n, x', y')])
+            else return ([], [(n, x', y')])
+    findNextJunction (x, y) vis b n =
+      let neigh = [ (x', y', b') | (x', y', c) <- [ (x, y-1, '^'), (x, y+1, 'v')
+                                                  , (x-1, y, '<'), (x+1, y, '>') ]
+                                , Just c' <- [mp V.!? y' >>= (V.!? x')]
+                                , c' /= '#', Set.notMember (x', y') vis
+                                , let b' = b && (c' == '.' || c' == c) ]
+      in case neigh of
+        [(x', y', b')]
+          | (x', y') `Set.member` junctions -> Just (x', y', b', n+2)
+          | otherwise -> findNextJunction (x', y') (Set.insert (x, y) vis) b' (n+1)
+        [] -> Nothing
+        xs -> error $ show (x, y, vis, xs)
+    junctions = foldr (Set.insert . swap . fmap fst) Set.empty $
+                  V.filter isJunction $ V.concat $ V.toList indexed
+    indexed = V.zipWith (\i -> fmap (i,) . V.indexed) (V.fromList [0..]) mp
+    isJunction (y, (x, c)) = c /= '#' && 2 /= length (
+      filter (/= Just True) [ fmap (/= '#') ((mp V.!? y) >>= (V.!? (x-1)))
+                            , fmap (/= '#') ((mp V.!? y) >>= (V.!? (x+1)))
+                            , fmap (/= '#') ((mp V.!? (y-1)) >>= (V.!? x))
+                            , fmap (/= '#') ((mp V.!? (y+1)) >>= (V.!? x))
+                            ])
+
 day23 :: IO ()
-day23 = return ()
+day23 = do
+  mp <- lines <$> readFile "input/day23"
+  case (elemIndex '.' (head mp), elemIndex '.' (last mp)) of
+    (Just startX, Just endX) -> do
+      let (startY, endY) = (0, length mp - 1)
+      let g = mkGraph (V.fromList $ map V.fromList mp)
+      putStr "Part 1: "
+      print $ findLongestPath @Slope g (startX, startY) (endX, endY)
+      putStr "Part 2: "
+      print $ findLongestPath @NoSlope g (startX, startY) (endX, endY)
+
+    _ -> print "No start/end found"
+
+data SlopeType = Slope | NoSlope
+
+class CheckSlope (a :: SlopeType) where
+  checkSlope :: (y, y) -> y
+
+instance CheckSlope 'Slope where
+  checkSlope (s, _) = s
+
+instance CheckSlope 'NoSlope where
+  checkSlope (_, s) = s
